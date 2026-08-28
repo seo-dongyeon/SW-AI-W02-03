e60fbd65e23184de4df67080f6e60838dca35868
seo-dongyeon <s.dongyeon.me@gmail.com>
2026-08-28 14:54:23 +0900

    Solve 03_python_dict

--- week2/1. basic/03_python_dict.py
+++ week2/1. basic/03_python_dict.py
@@ -42,13 +42,18 @@ def find_above_average_students(students):
         tuple: (평균 점수, 평균 이상 학생 이름 리스트)
     """
     # TODO: 모든 학생의 점수를 리스트로 추출하세요
-    pass
+    score = []
+    for i in students:
+        score.append(i["score"])
     
     # TODO: 평균 점수를 계산하세요
-    pass
+    average = sum(score)/len(score)
     
     # TODO: 평균 이상인 학생들의 이름을 리스트로 추출하세요
-    pass
+    above_average_students = []
+    for i in students:
+        if i["score"] >= average:
+            above_average_students.append(i["name"])
     
     return average, above_average_students
 
