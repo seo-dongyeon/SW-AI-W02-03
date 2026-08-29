38b45a65ef607caec4694dd76071ea5dedff8661
seo-dongyeon <s.dongyeon.me@gmail.com>
2026-08-29 14:24:58 +0900

    solve 11_divide_conquer

--- week2/1. basic/11_divide_conquer.py
+++ week2/1. basic/11_divide_conquer.py
@@ -36,19 +36,22 @@ def find_max_divide_conquer(arr, left, right):
         최댓값
     """
     # TODO: base case - 원소가 하나면 그 값 반환
-    pass
+    if left == right:
+        return arr[left]
     
     # TODO: 중간 지점 계산
-    pass
+    else:
+        mid = (left + right) // 2
     
     # TODO: 왼쪽 절반의 최댓값
-    pass
+        lmax = find_max_divide_conquer(arr, left, mid)
         
     # TODO: 오른쪽 절반의 최댓값
-    pass
+        rmax = find_max_divide_conquer(arr, mid+1, right)
     
     # TODO: 둘 중 큰 값 반환
-    pass
+        if lmax > rmax: return lmax
+        else: return rmax
 
 # 테스트 케이스
 if __name__ == "__main__":
